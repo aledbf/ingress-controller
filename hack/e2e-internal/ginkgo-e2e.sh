2af9701300901e5f147c9796bd674c85b7de7a97
Manuel de Brito Fontes <aledbf@gmail.com>
2016-11-05 15:22:46 -0200

    Execute ginkgo

--- hack/e2e-internal/ginkgo-e2e.sh
+++ hack/e2e-internal/ginkgo-e2e.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
+[[ $DEBUG ]] && set -x
+
+set -eof pipefail
+
+# include env
+. hack/e2e-internal/e2e-env.sh
+
 echo "running ginkgo"
+
+export PATH="${BASEDIR}/..":"${PATH}"
+ginkgo -r \
+    --ginkgo.flakeAttempts=2 \
+    --repo-root="${BASEDIR}/.." \
+    --prefix=e2e \
+    --clean-start=true \
+    "${@:-}"
