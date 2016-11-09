e69e44f0d2d7aa444f43e1d144d9235f7c8265d9
Manuel de Brito Fontes <aledbf@gmail.com>
2016-11-09 16:45:47 -0300

    Add e2e tests

--- hack/e2e-internal/ginkgo-e2e.sh
+++ hack/e2e-internal/ginkgo-e2e.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
+[[ $DEBUG ]] && set -x
+
+set -eof pipefail
+
+# include env
+. hack/e2e-internal/e2e-env.sh
+
 echo "running ginkgo"
+
+cd test/e2e
+ginkgo -r \
+    -keepGoing -- \
+    -kubeconfig=$HOME/.kube/config \
+    "${@:-}"
--- test/e2e/ingress_utils.go
+++ test/e2e/ingress_utils.go
@@ -25,6 +25,7 @@ import (
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"fmt"
+	"html/template"
 	"io"
 	"io/ioutil"
 	"math/big"
@@ -36,7 +37,6 @@ import (
 	"time"
 
 	"k8s.io/kubernetes/pkg/api"
-
 	apierrs "k8s.io/kubernetes/pkg/api/errors"
 	"k8s.io/kubernetes/pkg/apis/extensions"
 	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
@@ -466,27 +466,48 @@ func newTestJig(c clientset.Interface) *testJig {
 // NginxIngressController manages implementation details of Ingress on Nginx.
 type NginxIngressController struct {
 	ns         string
-	rc         *api.ReplicationController
+	dp         *extensions.Deployment
 	pod        *api.Pod
 	c          clientset.Interface
 	externalIP string
 }
 
 func (cont *NginxIngressController) init() {
-	mkpath := func(file string) string {
-		return filepath.Join(framework.TestContext.RepoRoot, ingressManifestPath, "nginx", file)
-	}
 	framework.Logf("initializing nginx ingress controller")
-	framework.RunKubectlOrDie("create", "-f", mkpath("rc.yaml"), fmt.Sprintf("--namespace=%v", cont.ns))
 
-	rc, err := cont.c.Core().ReplicationControllers(cont.ns).Get("nginx-ingress-controller")
+	tmpfile, err := ioutil.TempFile("", "nginx.yaml")
+	if err != nil {
+		panic(err)
+	}
+	defer tmpfile.Close()
+
+	rel := os.Getenv("RELEASE")
+	tmpl, err := template.New("deployment").Parse(dsTemplate)
+	if err != nil {
+		panic(err)
+	}
+	conf := make(map[string]interface{})
+	conf["image"] = fmt.Sprintf("quay.io/aledbf/nginx-ingress-controller:%v", rel)
+	conf["namespace"] = cont.ns
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, conf)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(tmpfile.Name(), buf.Bytes(), 0644)
+
+	framework.RunKubectlOrDie("create", "-f", tmpfile.Name(), fmt.Sprintf("--namespace=%v", cont.ns))
+
+	dp, err := cont.c.Extensions().Deployments(cont.ns).Get("nginx-ingress-controller")
 	ExpectNoError(err)
-	cont.rc = rc
+	cont.dp = dp
 
-	framework.Logf("waiting for pods with label %v", rc.Spec.Selector)
-	sel := labels.SelectorFromSet(labels.Set(rc.Spec.Selector))
+	framework.Logf("waiting for pods with label %v", dp.Spec.Selector)
+	sel := labels.SelectorFromSet(labels.Set(dp.Spec.Selector.MatchLabels))
 	ExpectNoError(testutils.WaitForPodsWithLabelRunning(cont.c, cont.ns, sel))
-	pods, err := cont.c.Core().Pods(cont.ns).List(api.ListOptions{LabelSelector: sel})
+	pods, err := cont.c.Core().Pods(cont.ns).List(api.ListOptions{
+		LabelSelector: sel,
+	})
 	ExpectNoError(err)
 	if len(pods.Items) == 0 {
 		framework.Failf("Failed to find nginx ingress controller pods with selector %v", sel)
@@ -495,4 +516,33 @@ func (cont *NginxIngressController) init() {
 	cont.externalIP, err = framework.GetHostExternalAddress(cont.c, cont.pod)
 	ExpectNoError(err)
 	framework.Logf("ingress controller running in pod %v on ip %v", cont.pod.Name, cont.externalIP)
+}
+
+// ExpectNoError ...
+func ExpectNoError(err error) {
+	Expect(err).NotTo(HaveOccurred())
+}
+
+// simpleGET executes a get on the given url, returns error if non-200 returned.
+func simpleGET(c *http.Client, url, host string) (string, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Host = host
+	res, err := c.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	rawBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	body := string(rawBody)
+	if res.StatusCode != http.StatusOK {
+		err = fmt.Errorf(
+			"GET returned http error %v", res.StatusCode)
+	}
+	return body, err
 }
